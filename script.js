218d792b60dc8d7178b3c0b6a98a09e8eef5c716
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-11 03:13:37 +0000

    comments

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let measurement = 0 
+let measurement = 0  /* creates variable 'measurement */
 
-document.getElementById('button').addEventListener('click', multiplication)
+document.getElementById('button').addEventListener('click', multiplication) /* makes action occur when button is clicked */
 
 function multiplication () {
-  measurement = document.getElementById('input').value
+  measurement = document.getElementById('input').value /* links the variable to the input in html file */
 
-  measurement = parseInt(measurement)
+  measurement = parseInt(measurement) /* records input as integer */
 
-  measurement = measurement * 4
+  measurement = measurement * 4 /* multiplies input for variable 'measurement' by 4 */
 
-  alert("The area of your square is " +  measurement)
-}
+  alert("The area of your square is " +  measurement) /* alerts results of the calculation, prints string 'The area of your square is ' with result of calculated value of the variable 'measurement */
+} 
